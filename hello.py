c3c828199026ca219c9ba1a2eb7b3693b506c277
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-08-16 23:06:51 +0100

    add first class

---
+++ hello.py
@@ -0,0 +1,23 @@
+name = "Moses"
+age = 45
+names = ["Moses", "John", "Jane"]
+details = {"name": "Moses", "age": 45, "phone": "123-456-7890"}
+
+print(names)
+names.append("Alice")
+
+print(names)
+
+# names.remove("John")
+# print(names)
+
+names.insert(1, "Bob")
+print(names)
+names.sort()
+print(names)
+ 
+
+# list []
+# dict {name: value, age: value, phone: value}
+# set
+# tuple 
